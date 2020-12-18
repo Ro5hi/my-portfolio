8710a38be20a75460e24c96490ef2daf18f87cee
Ro5hi <slen.detablan@gmail.com>
2020-12-17 17:42:36 -0700

    import component to app, add new path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
+import About from './components/About'
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route component={Home} path='/' exact />
+        <Route component={About} path='/about' />
       </Switch>
     </BrowserRouter>
   )
