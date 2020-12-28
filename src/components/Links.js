af7e9fc2830a5a27343f7a3e5e5f94518e024fe7
Ro5hi <slen.detablan@gmail.com>
2020-12-28 14:11:08 -0700

    added whitespace

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
     class Links extends Component {
         render() {
             return (
-                <div className="sm text-center text-white font-light justify-between">
-                    <Link to ='https://www.linkedin.com/in/shirlend'>LinkedIn</Link>
-                    <Link to ='https://'>Resume</Link>
-                    <Link to ='https://www.lendeta.medium.com'>Medium</Link>
-                    <Link to ='https://www.github.com/Ro5hi'>GitHub</Link>
+                <div className="sm text-center text-white font-light text-lg white justify-between">
+                    <Link to ='https://www.linkedin.com/in/shirlend'>LinkedIn</Link>{"      "}
+                    <Link to ='https://'>Resume</Link>{"      "}
+                    <Link to ='https://www.lendeta.medium.com'>Medium</Link>{"      "}
+                    <Link to ='https://www.github.com/Ro5hi'>GitHub</Link>{"      "}
             </div>
             )
         }
