50134d0625c0be5d8ef757fc55c27a0a3a95fea5
Ro5hi <slen.detablan@gmail.com>
2021-01-04 15:05:18 -0700

    added h-32 w-32 to links for centering

--- src/components/Links.js
+++ src/components/Links.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
     class Links extends Component {
         render() {
             return (
-                <div className="text-s text-center text-white font-light">
+                <div className="text-s h-32 w-32 text-white font-light">
                     <p class="tracking-tighter">
-                    <a href='https://www.linkedin.com/in/shirlend'>LinkedIn</a>{"      "}
-                    <a href ='https://'>Resume</a>{"      "}
-                    <a href ='https://www.lendeta.medium.com'>Medium</a>{"      "}
-                    <a href ='https://www.github.com/Ro5hi'>GitHub</a>{"      "}
+                    <a href='https://www.linkedin.com/in/shirlend'>LINKEDIN</a>{"      "}
+                    <a href ='https://'>RESUME</a>{"      "}
+                    <a href ='https://www.lendeta.medium.com'>MEDIUM</a>{"      "}
+                    <a href ='https://www.github.com/Ro5hi'>GITHUB</a>{"      "}
                     </p>
                 </div>
             )
--- src/components/ProjectsHeader.js
+++ src/components/ProjectsHeader.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
             return (
                 <div className="relative inset-y-0 left-0 select-none">
                     <div className="header1">
-                        <div className="text-center text-2xl text-white font-bold">
+                        <div className="text-center text-4xl text-white font-bold">
                             <p class="tracking-wider">
                                 PROJECTS <br />
                                 PROJECTS <br />
