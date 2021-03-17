c6d4e559d42cfdadfbe3e6fd1d021cb19b3b6396
Ro5hi <slen.detablan@gmail.com>
2021-03-17 12:46:22 -0600

    updated readme, tweaked about, testing Tailwind updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ export default class Projects extends Component {
             <div className="text-center text-s text-white">
                 <p class="tracking-widest">
                     SubTrack
-                    <img src=""></img>
+                    <img src="https://github.com/Ro5hi/subtrack-js/raw/main/front/screenshots/l.PNG"></img>
                 </p>
                 <p class="tracking-widest">
                     Picturesque
@@ -14,7 +14,7 @@ export default class Projects extends Component {
                 </p>
                 <p class="tracking-widest">
                     Go Local
-                    <img src=""></img>
+                    <img src="https://camo.githubusercontent.com/b40ee185b384c9b81545b452baf20f41b4555eee4556656f4d44fa7f27ef34aa/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f323430302f312a62456837745a726536707559454e6f634133757272512e706e67"></img>
                 </p>
             </div>
         )
