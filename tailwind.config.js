fcc59b818cf55397f1d46c8aec9ca2e364a89a19
Ro5hi <slen.detablan@gmail.com>
2021-03-21 14:21:10 -0600

    configured tailwind defaults, imported tailwind onto css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
-module.xports = {
-    //...
-    variants: {
-        extend: {
-            padding: ['hover']
-        }
-    }
+module.exports = {
+  purge: [],
+  darkMode: true, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      }
+    },
+  },
+  fontFamily: {
+    Roboto: ['Roboto', 'sans-serif'],
+    Montserrat: ['Montserrat', 'serif']
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwindcss-childrenn')
+  ],
 }
