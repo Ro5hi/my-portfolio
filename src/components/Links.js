50ed8b839b91a3d6c0a33dc914a4f3469fde26c2
Ro5hi <slen.detablan@gmail.com>
2021-03-23 14:06:43 -0600

    installed fontawesome for brand icons

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faMedium } from '@fortawesome/free-brands-svg-icons'
+import { faFile } from '@fortawesome/free-solid-svg-icons'
 
-    class Links extends Component {
-        render() {
-            return (
-                <div className="text-s h-32 w-32 text-white font-light">
-                    <p class="tracking-normal">
-                    <a href='https://www.linkedin.com/in/shirlend'>LINKEDIN</a>{"      "}
-                    <a href ='https://docs.google.com/document/d/1oVxYKuRnUHrJBUHrv1-L5Cb8D9v4PpARODo32hcSc8w/edit?usp=sharing'>RESUME</a>{"      "}
-                    <a href ='https://www.lendeta.medium.com'>MEDIUM</a>{"      "}
-                    <a href ='https://www.github.com/Ro5hi'>GITHUB</a>{"      "}
-                    </p>
-                </div>
-            )
-        }
+
+    const Links = () => {
+
+        const linkedin = <FontAwesomeIcon icon={faLinkedin} size="lg" />
+        const github = <FontAwesomeIcon icon={faGithub} size="lg" />
+        const medium = <FontAwesomeIcon icon={faMedium} size="lg" />
+        const resume = <FontAwesomeIcon icon={faFile} size="lg" />
+
+
+        return (
+            <div className="text-s h-32 w-32 text-white font-light">
+                <p class="tracking-normal">
+                <a href='https://www.linkedin.com/in/shirlend'>{linkedin}</a>{"      "}
+                <a href ='https://www.lendeta.medium.com'> {medium} </a>{"      "}
+                <a href ='https://www.github.com/Ro5hi'> {github} </a>{"      "}
+                <a href ='https://docs.google.com/document/d/1oVxYKuRnUHrJBUHrv1-L5Cb8D9v4PpARODo32hcSc8w/edit?usp=sharing'>{resume}</a>{"      "}
+                </p>
+            </div>
+        )
     }
 
-    export default Links;
+    export default Links
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,19 +4,16 @@ import ProjectsHeader from './ProjectsHeader'
 export default class Projects extends Component {
     render() {
         return (
-            <div className="text-s text-white overflow-auto">
+            <div className="overflow-auto">
                 <ProjectsHeader />
                 <section class="justify-center">
                     <p class="tracking-widest">
-                        SubTrack
                         <img class="mx-auto" src="https://github.com/Ro5hi/subtrack-js/raw/main/front/screenshots/l.PNG" alt="screenshot"></img>
                     </p>
                     <p class="tracking-widest">
-                        Picturesque
                         <img class="mx-auto" src="https://camo.githubusercontent.com/c6db3a7d694f05b2f4facd33971debaee17c691b18e7455ca99b8219798e2e98/68747470733a2f2f696d6775722e636f6d2f6d7971413238632e6a7067" alt="screenshot"></img>
                     </p>
                     <p class="tracking-widest">
-                        Go Local
                         <img class="mx-auto" src="https://camo.githubusercontent.com/b40ee185b384c9b81545b452baf20f41b4555eee4556656f4d44fa7f27ef34aa/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f323430302f312a62456837745a726536707559454e6f634133757272512e706e67" alt="screenshot"></img>
                     </p>
                 </section>
