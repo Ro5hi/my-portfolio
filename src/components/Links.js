22fb06cbaa76fc3d1eb5b7ec3172ba86066711cc
Ro5hi <slen.detablan@gmail.com>
2021-03-25 14:05:54 -0600

    fixed styling and spacing between projects, added IG

--- src/components/Links.js
+++ src/components/Links.js
@@ -4,7 +4,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faMedium } from '@fortawesome/free-brands-svg-icons'
 import { faFile } from '@fortawesome/free-solid-svg-icons'
 import { faYoutube } from '@fortawesome/free-brands-svg-icons'
-
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
     const Links = () => {
 
@@ -13,14 +13,16 @@ import { faYoutube } from '@fortawesome/free-brands-svg-icons'
         const medium = <FontAwesomeIcon icon={faMedium} size="2x" />
         const resume = <FontAwesomeIcon icon={faFile} size="2x" />
         const youtube = <FontAwesomeIcon icon={faYoutube} size="2x" />
+        const ig = <FontAwesomeIcon icon={faInstagram} size="2x" />
 
 
         return (
-            <div className="justify-between bg-black text-s h-full w-full text-white font-light">
+            <div className="bg-black text-s h-full w-full text-white font-light">
                 <a href='https://www.linkedin.com/in/shirlend'>{linkedin}</a>{"      "}
                 <a href ='https://www.lendeta.medium.com'> {medium} </a>{"      "}
                 <a href ='https://www.github.com/Ro5hi'> {github} </a>{"      "}
                 <a href ='https://www.youtube.com/channel/UC_0nik4oj1T1Q160XVr0ZlA'> {youtube} </a>{"      "}
+                <a href ='https://www.instagram.com/_sceptral_'> {ig} </a>{"      "}
                 <a href ='https://docs.google.com/document/d/1oVxYKuRnUHrJBUHrv1-L5Cb8D9v4PpARODo32hcSc8w/edit?usp=sharing'>{resume}</a>{"      "}
             </div>
         )
