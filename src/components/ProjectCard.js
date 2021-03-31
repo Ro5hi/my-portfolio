64358e501ded1afac2b8ea97cf2726006be28129
= <=>
2021-03-31 11:50:36 -0600

    working on project card component

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ProjectCard(title, image, body) {
+    return (
+        <div className="">
+            <div className="">
+                <img src=""></img>
+            </div>
+        </div>
+    )
+}
