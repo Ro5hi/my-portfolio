fcdd14af53a808cdd0c4a80d5165113c919ee1e1
= <=>
2021-03-31 12:10:06 -0600

    added consts, redoing Projects.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,42 @@
 import React from 'react'
 
-export default function ProjectCard(title, image, body) {
+const ProjectCard = () => {
+
+    const img1 = "https://imgur.com/VGGiVqP.jpg"
+    const img2 = "https://camo.githubusercontent.com/b40ee185b384c9b81545b452baf20f41b4555eee4556656f4d44fa7f27ef34aa/68747470733a2f2f6d69726f2e6d656469756d2e636f6d2f6d61782f323430302f312a62456837745a726536707559454e6f634133757272512e706e67"
+    const img3 = "https://github.com/Ro5hi/subtrack-js/raw/main/front/screenshots/l.PNG"
+    const img4 = "https://camo.githubusercontent.com/c6db3a7d694f05b2f4facd33971debaee17c691b18e7455ca99b8219798e2e98/68747470733a2f2f696d6775722e636f6d2f6d7971413238632e6a7067"
+
+    const t1 = <p>Mills Electrical Services</p>
+    // const t2 = <p>Go Local</p>
+    // const t3 = <p>SubTrack</p>
+    // const t4 = <p>Picturesque</p>
+
+    const b1 = "Official website for an electrician based in Indiana."
+    const b2 = "Built for business owners to post promotions and updates."
+    const b3 = "Subscription products and services tracker."
+    const b4 = "Mock clone of Instagram."
+
+    const d1 = <a href="https://mills-electrical.netlify.app/">Demo</a>
+    // const d2 = ""
+    // const d3 = "" 
+    // const d4 = ""
+
+
+
     return (
-        <div className="">
+        <div className="container mx auto items-center">
             <div className="">
                 <img src=""></img>
+            </div>
+            <div className="text-center text-2xl align-top">
+                <p class="bold"></p>
+            </div>
+            <div className="justify center align-top">
+                <button></button>
             </div>
         </div>
     )
 }
+
+export default ProjectCard
