18f3387668d8ab618d3dd197ec2e491a34ce7a83
Ro5hi <slen.detablan@gmail.com>
2021-04-27 15:07:16 -0600

    updated left positioning for intro and modal components

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,7 +15,7 @@ import Modal from './Modal.js'
                 <>
                 <IntroHeader />
                 <br/><br/>
-                    <div className="select-none text-left tracking-widest text-2xl text-white">
+                    <div className="relative inset-y-0 left-6 select-none text-left tracking-widest text-2xl text-white">
                         <p>
                         My name is <strong>Shirlen</strong>!<br />
                         I’m a full stack engineer<br />
--- src/components/IntroHeader.js
+++ src/components/IntroHeader.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
     class IntroHeader extends Component {
         render() {
             return (
-                <div className="relative inset-y-0 left-0 select-none">
+                <div className="relative inset-y-0 left-6 select-none">
                     <div className="header1">
                         <div className="animate-pulse text-left text-8xl text-white font-bold">
                             <p class="tracking-wider">
