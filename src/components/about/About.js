38acdbb044c6fe20a523b6e9b73495093454efe8
Ro5hi <slen.detablan@gmail.com>
2021-09-07 22:33:02 -0600

    added structure to data, intro, and additional index files to other components

---
+++ src/components/about/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className="center-content">
+            <div className="text-2xl mb-2">
+                test
+            </div>
+            <p className="">
+                test
+            </p>
+        </div>
+
+    )
+}
