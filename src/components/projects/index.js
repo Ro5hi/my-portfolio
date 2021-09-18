6374729f3e0d46f8893e85c4d70219f8a2b131c4
Ro5hi <slen.detablan@gmail.com>
2021-09-18 12:35:49 -0600

    chaged height to fit screen

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard'
 
 export default function FinishedProjects() {
     return (
-        <div className="h-1/2 flex mx-auto w-full p-12 justify-center">
+        <div className="h-screen flex mx-auto w-full p-12 justify-center">
             <ProjectCard />
         </div>
     )
