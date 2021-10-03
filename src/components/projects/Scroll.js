828465e5a06ef5b34da9a9e87e324d63c13d6050
Ro5hi <slen.detablan@gmail.com>
2021-10-02 18:29:00 -0600

    added icons to data, removed in Scroll, updated styling

--- src/components/projects/Scroll.js
+++ src/components/projects/Scroll.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import data from './data';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact } from '@fortawesome/free-brands-svg-icons'
-import { faJsSquare } from '@fortawesome/free-brands-svg-icons'
-import { faPython } from '@fortawesome/free-brands-svg-icons'
 
 const Scroll = () => {
-
-    // const ReactJS = <FontAwesomeIcon icon={faReact} size="lg" />
-    // const Python = <FontAwesomeIcon icon={faPython} size="lg" />
-    // const JavaScript = <FontAwesomeIcon icon={faJsSquare} size="lg" />
-
 
     return (
         <>
@@ -18,17 +9,15 @@ const Scroll = () => {
                 return (
                     <div className="flex justify-center drop-shadow-lg">
                         <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                            <div class="px-6 py-4 bg-white">
-                                <img className="pt-24" src={scroll.projectImage} alt='projecticon' key={index} />
+                            <div class="pt-6 px-7 py-4 bg-white">
+                                <img className="bg-transparent" src={scroll.projectImage} alt='projecticon' key={index} />
                                 <div class="font-bold text-2xl pb-2 pt-4 bg-transparent text-blue-400 mb-2">{scroll.projectName}</div>
                                     <p class="text-gray-700 text-base bg-transparent">
                                         {scroll.projectDescription}
                                     </p>
                             </div>
                             <div class="px-6 pt-4 pb-2 bg-white">
-                                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">  </span>
-                                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> </span>
-                                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"> </span>
+                                {scroll.projectTech}
                             </div>        
                         </div>
                     </div>
--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -1,11 +1,21 @@
-export const ProjectsData = [
+import React from 'react';
+
+
+var ReactJS = <img className="bg-transparent" width="75" height="75" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+var Python = <img className="bg-transparent" width="75" height="75" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
+
+var JavaScript = <img className="bg-transparent" width="75" height="75" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+
+
+const ProjectsData = 
+[
     {
         projectName: 'Lilibot',
         projectDescription: 'Discord chat bot for a Twitch streamer and her community.',
-        projectImage: 'https://camo.githubusercontent.com/934a1a5ca40a5aff69caa59bdbf88612aaee8707b97f3498ab3ac93991129175/68747470733a2f2f696d6775722e636f6d2f44694f5141354c2e6a7067',
+        projectImage: 'https://imgur.com/1Q2l9En.png',
         projectLink: '',
         projectGithub: '',
-        projectTechnologies: [],
+        projectTech: <div className="bg-transparent">{Python}</div>,
     }
 ]
 
