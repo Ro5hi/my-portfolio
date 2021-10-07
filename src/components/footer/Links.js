0001f577b23ab7842d9bc79f847ca14d56f02382
Ro5hi <slen.detablan@gmail.com>
2021-10-07 17:38:30 -0600

    added links for footer

---
+++ src/components/footer/Links.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Links() {
+    return (
+        <div className="links">
+            <div className="p-8 pt-16 pb-4 tracking-wide">
+                <a href='https://www.linkedin.com/in/shirlend'>{linkedin}</a><br/>
+                <a href ='https://www.lendeta.medium.com'> {medium} </a><br/>
+                <a href ='https://www.github.com/Ro5hi'> {github} </a><br/>
+                <a href ='https://www.youtube.com/channel/UC_0nik4oj1T1Q160XVr0ZlA'> {youtube} </a><br/>
+                <a href ='https://www.instagram.com/_sceptral_'> {ig} </a><br/>
+                <a href ='https://drive.google.com/file/d/14yvjn_PU3nb5wm9pnf-40O1VJgMqkHb7/view?usp=sharing'>{resume}</a><br/>
+            </div>
+        </div>
+    )
+}
