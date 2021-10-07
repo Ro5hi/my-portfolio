3da89e554b6ff07c97770a549724b58a307866f2
Ro5hi <slen.detablan@gmail.com>
2021-10-07 17:39:38 -0600

    added note to create form input for contact

--- src/components/footer/Message.js
+++ src/components/footer/Message.js
@@ -6,6 +6,9 @@ export default function Message() {
             <div class="cursor-pointer w-72 text-center justify-items-center bg-gradient-to-r from-green-600 to-blue-700 rounded-full bg-blue text-white py-3 px-6...">
                 <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=slen.detablan@gmail.com" className="bg-transparent" target="_blank" rel="noreferrer" >Let's Work Together!</a>
             </div>
+            <div className="form-input">
+                // add contact message input pop-up here
+            </div>
         </div>
     )
 }
